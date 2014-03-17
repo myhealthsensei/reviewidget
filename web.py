33d67b2411eabab0ec4a223920935fc31653b222
Japhy Bartlett <japherwocky@localdev>
2014-03-17 16:20:18 -0500

    refactor db stuff into a different file

--- web.py
+++ web.py
@@ -19,8 +19,8 @@ class MainHandler(tornado.web.RequestHandler):
 
         self.render('main.html', **outs)
 
-
-class App(tornado.web.Application):
+from db import DB
+class App(DB, tornado.web.Application):
 
     """ 
         The Application is usually a singleton per process,
@@ -51,127 +51,15 @@ class App(tornado.web.Application):
             (r"/logout/?", Logout),
         ]
 
-
         self.init_db()
 
+        # fixtures if we want them
         if seed:
             self.seed()
 
         # let tornado __init__ whatever it needs
         tornado.web.Application.__init__(self, handlers, **settings)
 
-    def init_db(self):
-        """ 
-        Connect to database, create tables if they don't exist .. 
-        stash this code somewhere else at some point as a mixin
-        """
-
-        logging.info("Initializing database..")
-
-        import os
-        import urlparse
-
-        import psycopg2
-        import psycopg2.extras
-
-        if "DATABASE_URL" in os.environ:
-            urlparse.uses_netloc.append("postgres")
-            url = urlparse.urlparse(os.environ["DATABASE_URL"])
-
-            self.db = psycopg2.connect(
-                database=url.path[1:],
-                user=url.username,
-                password=url.password,
-                host=url.hostname,
-                port=url.port
-            )
-
-        else:
-            logging.error("No $DATABASE_URL found in environ, ABORTING STARTUP")
-            os.exit(1)
-
-        # build connections to db
-        self.cursor = self.db.cursor(cursor_factory = psycopg2.extras.DictCursor)
-        cursor = self.cursor
-
-        queries = {
-            'resources': """CREATE TABLE IF NOT EXISTS resources (
-                id BIGSERIAL PRIMARY KEY,
-                slug VARCHAR(50), 
-                name VARCHAR(100), 
-                email VARCHAR(100), 
-                phone VARCHAR(100), 
-                description TEXT, 
-                logo VARCHAR(100)
-                )""",
-
-            'reviews': """CREATE TABLE IF NOT EXISTS reviews (
-                id BIGSERIAL PRIMARY KEY,
-                resource_id INT, 
-                author_id INT, 
-                role VARCHAR(100), 
-                rating INT, 
-                review TEXT
-                )""",
-
-            'authors': """CREATE TABLE IF NOT EXISTS authors (
-                id BIGSERIAL PRIMARY KEY,
-                name VARCHAR(100), 
-                login VARCHAR(100), 
-                passhash VARCHAR(100), 
-                admin BOOL, 
-                email VARCHAR(100)
-                )""",
-
-            'tags': """CREATE TABLE IF NOT EXISTS tags (
-                id BIGSERIAL PRIMARY KEY,
-                slug VARCHAR(100), 
-                human VARCHAR(100)
-                )""",
-
-            # m2m table linking tags and reviews
-            'tags_reviews': """CREATE TABLE IF NOT EXISTS tags_reviews (
-                tag_id INT, 
-                review_id INT
-                )""",
-            # sessions when that comes
-        }
-
-
-        for table in queries:
-            logging.debug( 'CREATING {}'.format(table))
-            cursor.execute( queries[table])
-
-        self.db.commit()
-
-
-    def seed(self):
-        """ Pump in some placeholder data for dev work / testing """
-
-        from random import choice
-
-        cursor = self.db.cursor()
-
-        # stash dummy content here
-        self.content = json.loads(open('static/alice.json').read())
-
-        # we have hash/content
-        for key in self.content:
-            slug = key
-            description = self.content[key]
-
-            words = self.content[key].split()[1:-2]
-
-            name = choice(words).capitalize() + ' ' + choice(words).capitalize()
-            email = 'nobody@nowhere.com'  # should choose some blanks, whatevs
-            phone = '312-555-1212'
-
-            logo = 'http://lorempixel.com/200/200/animals/' 
-
-            cursor.execute( "INSERT INTO resources (slug, name, email, phone, description, logo) VALUES (%s,%s,%s,%s,%s,%s)", 
-                            (slug, name, email, phone, description, logo))
-
-        self.db.commit()
 
     def adduser(self, login):
         """ util method to add users from commandline, good for bootstrapping the first user """
