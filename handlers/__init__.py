c7d117f18343c035605ab81b31d31c89d13ca3f1
Japhy Bartlett <japherwocky@localdev>
2014-03-17 15:49:33 -0500

    sketch in base handler with methods for @authenticated

---
+++ handlers/__init__.py
@@ -0,0 +1,37 @@
+import tornado.web
+
+class BaseHandler(tornado.web.RequestHandler):
+
+    @property
+    def db(self): 
+        """ convenience shortcut """
+        return self.application.db
+
+
+    _cursor = None
+
+    @property
+    def cursor(self):
+        """Create a new cursor per request"""
+        if not self._cursor:
+            self._cursor = self.db.cursor()
+
+        return _cursor
+
+
+    _user = None
+
+    @property
+    def user(self): return self.get_current_user()
+
+    def get_current_user(self):
+        if not self._user:
+            login = self.get_secure_cookie('user')
+            if not login:
+                return None
+
+            self.cursor.execute( "SELECT * FROM authors WHERE login=%s", (login,))
+            self._user = self.cursor.fetchone()
+
+        return self._user
+
